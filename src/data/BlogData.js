344e584ea92fb2788348c8c9f66eb819a5a66164
Muhammad Usman <bytesofusman@gmail.com>
2023-03-04 16:59:30 +0500

    Add files via upload

---
+++ src/data/BlogData.js
@@ -0,0 +1,46 @@
+// blog data
+
+export const Blogs = [
+    {
+        id: 1,
+        name: "Laptop Monitoring Tool React",
+        tags: ["react", "flask", "dashboard"],
+        date: "4 March, 2023",
+        imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--FYrOKMva--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ujphd5psls9bceezckjb.png",
+        link: "https://dev.to/bytesofusman/dashboard-for-laptop-monitoring-1oeo"
+    },
+    {
+        id: 2,
+        name: "Hack a Machine",
+        tags: ["hacking", "sqlmap", "cyber"],
+        date: "19 Feb, 2023",
+        imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--YCMc_eh9--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0bo84wxzynl277173n9y.jpg",
+        link: "https://dev.to/bytesofusman/hack-a-machine-3mh8"
+    },
+    {
+        id: 3,
+        name: "Hack a Machine",
+        tags: ["hacking", "sqlmap", "cyber"],
+        date: "19 Feb, 2023",
+        imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--YCMc_eh9--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0bo84wxzynl277173n9y.jpg",
+        link: "https://dev.to/bytesofusman/hack-a-machine-3mh8"
+    },
+    {
+        id: 4,
+        name: "Hack a Machine",
+        tags: ["hacking", "sqlmap", "cyber"],
+        date: "19 Feb, 2023",
+        imgSrc: "https://res.cloudinary.com/practicaldev/image/fetch/s--YCMc_eh9--/c_imagga_scale,f_auto,fl_progressive,h_420,q_auto,w_1000/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/0bo84wxzynl277173n9y.jpg",
+        link: "https://dev.to/bytesofusman/hack-a-machine-3mh8"
+    },
+
+
+]
+// {
+//     id: ,
+//     name: "",
+//     tags: [],
+//     date: "",
+//     imgSrc: "",
+//     link: ""
+// },
---
+++ src/data/WorkData.js
@@ -0,0 +1,25 @@
+// work data
+
+export const Work = [
+    {
+        id: 1,
+        name: "Ip Scanner",
+        description: "An recon tool for finding IP addresses behind cloudflare.",
+        tags: ["django", "ip", "cloudflare", "recon"],
+
+        demo: "https://github.com/ipisbest/thebestwithdjango/",
+        github: "https://github.com/ipisbest/thebestwithdjango"
+    },
+    {
+        id: 2,
+        name: "Studybud",
+        description: "Django Web Application",
+        tags: ["chating", "django"],
+
+        demo: "http://studybud.pythonanywhere.com/",
+        github: "https://github.com/codeninjausman/ChatingSpace"
+
+    },
+
+
+] 
